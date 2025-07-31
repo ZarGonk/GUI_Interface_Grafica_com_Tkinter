7c9ce2a8dc07341d1c34232af8000d1e3961c927
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-07-31 18:33:51 -0300

    Add window icon to Tkinter application
    Created a new Tkinter window and set a custom icon using 'iconbitmap'. The window title and geometry are also specified.

---
+++ 1.3 Alterando Icon da Janela.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Main Window
+root = tk.Tk()
+root.title('Novo Icon')
+root.geometry('300x200+50+50')
+
+# Charge the window icon
+root.iconbitmap('assets/pythontutorial-1-150x150.ico')
+
+root.mainloop()
