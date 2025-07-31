2e0c55ce0f01879de2b1c19df33fdf7a221db39a
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-07-31 18:34:04 -0300

    Add basic Tkinter window and icon asset
    Introduces a simple Tkinter window with title, geometry, and background color settings. Also adds a Python tutorial icon to the assets directory for potential use in the application.

---
+++ 2.1 Basic Window Settings.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+# Main Window
+root = tk.Tk()
+root.title('Basic Window')
+root.geometry('300x200+600+300')
+
+# Sets background the window
+root.config(background='lightblue')
+
+
+
+
+root.mainloop()
