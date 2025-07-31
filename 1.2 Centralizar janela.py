cd7645c1c95156d502cf33160dc5f7a11fc7e6e7
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-07-31 18:33:45 -0300

    Add script to center Tkinter window
    Introduces a Python script that creates a Tkinter window and centers it on the screen using calculated geometry.

---
+++ 1.2 Centralizar janela.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Centralizando Janela')
+
+window_width = 400
+window_height = 200
+
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_heigth = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width/2 - window_width/2)
+center_y = int(screen_heigth/2 - window_height/2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+
+root.mainloop()
