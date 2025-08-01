f7fa65ad782901cc81484958aa27faa7fa84dbbf
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-08-01 17:10:53 -0300

    Placing an image in the window

---
+++ 3. Imagem_na_Janela.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+# Main Window
+root = tk.Tk()
+root.geometry('300x200') # Define um tamanho para a tela 
+root.title('Mostrando Imagem')
+
+# Create Label Text
+message = tk.Label(
+    root,
+    text='Image below',
+    font= ('Times New Roman', 14),
+    fg='red'
+)
+message.pack()
+
+# Create Label Photo
+photo = tk.PhotoImage(file='GUI/assets/python.png')
+image_label = tk.Label(
+    root, 
+    image=photo,
+    text='Python',
+    compound=tk.TOP
+)
+image_label.pack()
+
+root.mainloop()
