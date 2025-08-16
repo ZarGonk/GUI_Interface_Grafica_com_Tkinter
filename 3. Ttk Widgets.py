48080e347b6de973300056ba4fad2c21c703161e
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-08-16 16:14:22 -0300

    Remove unnecessary button variable assignment
    Replaced the assignment of the packed Label widget to the 'button' variable with a direct call, as the variable was unused.

--- 3. Ttk Widgets.py
+++ 3. Ttk Widgets.py
@@ -4,8 +4,7 @@ from tkinter import ttk
 root = tk.Tk()
 root.geometry('300x200')
 
-button = tk.Label(root,text='Classic Label').pack()
+tk.Label(root, text='Classic Label').pack()
 ttk.Label(root, text='Themed Label').pack()
-
 
 root.mainloop()
