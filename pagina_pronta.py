b336ea612942344c3a63f90a974a017399ae6ed7
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-09-22 18:21:45 -0300

    Add initial Tkinter window setup
    Created pagina_pronta.py with a basic Tkinter window, including title, size, and resizable settings. This sets up the foundation for further GUI development.

---
+++ pagina_pronta.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Main Window
+root = tk.Tk()
+root.title('CheckButton 1')
+root.geometry('300x200')
+root.resizable(True, True) 
+
+
+
+
+# Start the Tkinter event loop
+root.mainloop()
