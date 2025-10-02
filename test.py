c7a18126b5ba96fdbccadc07bcfc4924748e7127
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-10-02 14:24:16 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,13 @@
-import tkinter as tk
-from tkinter import ttk
+def show_count(start=0, end=0, move=0):
+    try:
+        one = int(start)
+        two   = int(end)
+        three  = int(move)
 
-root = tk.Tk()
+        for i in range(one, two, three):
+            print(i)
+    except:
+        print('Valor Invalido')
+    
 
-# Criar um estilo personalizado
-style = ttk.Style()
-style.theme_use("clam")  # "clam" permite mais personalização
-
-# Configurar o estilo com cores específicas
-style.configure("Custom.TButton",
-                background="lightblue",
-                foreground="black",
-                borderwidth=1,
-                focusthickness=3,
-                focuscolor='none')
-
-style.map("Custom.TButton",
-          background=[("active", "blue"), ("pressed", "green")],
-          foreground=[("active", "white"), ("pressed", "black")])
-
-
-# Aplicar o estilo ao botão
-btn = ttk.Button(root, text="Clique aqui", style="Custom.TButton")
-btn.pack(padx=20, pady=20)
-
-
-root.mainloop()
+show_count('bom dia', 10+1, 2)
