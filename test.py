e5c0c00ba485e7cd251ee542f0f46c9aba53dbf7
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-11-11 00:04:00 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,5 @@
-class Pessoa:
-    def __init__(self, altura, idade):
-        self.altura = altura
-        self.idade = idade
-        self.running = False
-        self.eating = False
+import soma
 
+total = soma.soma(5, 6)
 
-    def run(self):
-        if self.running:
-            print("it's Already running")
-        elif not self.eating:
-            self.running = True
-            print('Run')
-        else:
-            self.running = False
-            print('You cant run, you are eating')
-
-    def stop_run(self):
-        self.running = False
-        print('Stopped Running')
-    
-
-    def eat(self):
-        if self.eating:
-            print("it's already eating")
-        elif not self.running:
-            self.eating = True
-            print('Eating')
-        else:
-            self.eating = False
-            print('You cant eat, you are running')
-        
-    def stop_eat(self):
-        self.eating = False
-        print('Stopped eating')
-    
-
-andry = Pessoa(1.58, 23)
-andry.run()
-andry.eat()
-andry.run()
-andry.stop_run()
-andry.eat()
-andry.eat()
-andry.stop_eat()
+print(total)
