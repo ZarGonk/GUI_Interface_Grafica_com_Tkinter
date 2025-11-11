52dc4416521290afd09d1c46d86d4646c09b4db8
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-11-10 23:08:07 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,46 @@
-def show_count(start=0, end=0, move=0):
-    try:
-        one = int(start)
-        two   = int(end)
-        three  = int(move)
+class Pessoa:
+    def __init__(self, altura, idade):
+        self.altura = altura
+        self.idade = idade
+        self.running = False
+        self.eating = False
 
-        for i in range(one, two, three):
-            print(i)
-    except:
-        print('Valor Invalido')
+
+    def run(self):
+        if self.running:
+            print("it's Already running")
+        elif not self.eating:
+            self.running = True
+            print('Run')
+        else:
+            self.running = False
+            print('You cant run, you are eating')
+
+    def stop_run(self):
+        self.running = False
+        print('Stopped Running')
     
 
-show_count(0, 11, 2)
+    def eat(self):
+        if self.eating:
+            print("it's already eating")
+        elif not self.running:
+            self.eating = True
+            print('Eating')
+        else:
+            self.eating = False
+            print('You cant eat, you are running')
+        
+    def stop_eat(self):
+        self.eating = False
+        print('Stopped eating')
+    
+
+andry = Pessoa(1.58, 23)
+andry.run()
+andry.eat()
+andry.run()
+andry.stop_run()
+andry.eat()
+andry.eat()
+andry.stop_eat()
