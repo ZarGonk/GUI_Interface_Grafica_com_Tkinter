b2d724698bb1cf88d455965e7ad35f2a2bbf3cce
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-11-25 22:57:52 -0300

    Add messagebox examples and main app logic
    Added buttons to display error, info, and warning messageboxes in the main app. Also created a placeholder for askyesno example. The main block now instantiates and runs the App.

--- 10. Messagebox/1. Messagebox.py
+++ 10. Messagebox/1. Messagebox.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.messagebox import showerror, showinfo, showwarning
 
 
 class App(tk.Tk):
@@ -9,11 +10,17 @@ class App(tk.Tk):
         self.title('Play')
         self.geometry('300x200')
 
+        option = {'fill': 'both', 'padx': 10, 'pady': 10, 'ipadx': 5 }
+
+        self.button_1 = ttk.Button(self, text='Show an error', command=lambda: showerror(title='Error', message='This is an erro')).pack(**option)
+
+        self.button_2 = ttk.Button(self, text='Show an information', command=lambda: showinfo(title='Information', message='This is an Information')).pack(**option)
+
+        self.button_3 = ttk.Button(self, text='Show an warning', command=lambda: showwarning(title='Warning', message='This is an Warning')).pack(**option)
+
+
 
 if __name__ == "__main__":
-    pass
-
-
-
-
+    app = App()
+    app.mainloop()
 
