327c73159a566523779bdc420b2a0a07cec9bec1
Gustavo Rodrigues <gustavo.barreto013@hotmail.com>
2025-11-25 21:34:04 -0300

    Create 1. Messagebox.py

---
+++ 10. Messagebox/1. Messagebox.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Play')
+        self.geometry('300x200')
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
